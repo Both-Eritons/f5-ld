5450c5885c44b070d3b55cd955f86b3a25283923
Both-Eritons <both-eritons@outlook.com>
2023-09-03 15:56:42 -0300

    ...

--- _cdn/js/main.js
+++ _cdn/js/main.js
@@ -1,9 +1,23 @@
-const cards = document.querySelectorAll(".plans-click");
+function accordionClick() {
+	const cards = document.querySelectorAll(".plans-click");
 
-cards.forEach((card) => {
-	card.addEventListener("click", (i) => {
-		console.log(card.nextElementSibling)
-		card.nextElementSibling.classList.toggle("opened");
-		card.querySelector("i").classList.toggle("fa-angle-down");
+	cards.forEach((card) => {
+		card.addEventListener("click", (i) => {
+			card.nextElementSibling.classList.toggle("opened");
+			card.querySelector("i").classList.toggle("fa-angle-down");
+		})
 	})
-})
+}
+
+function navbar() {
+	const menu = document.querySelector(".nav-items");
+	const click = document.querySelector(".button-nav");
+
+	click.addEventListener("click", () => {
+		click.classList.toggle("clicked");
+		menu.classList.toggle("clicked");
+	})
+}
+
+navbar();
+accordionClick();
